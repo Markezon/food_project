50bd469ff33ce13ec400825d020b5b1118f1f44a
Dmitry <neopatogen@gmail.com>
2024-10-25 05:02:56 +0200

    add tabs animation by js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,32 +1,29 @@
+/******/ (() => { // webpackBootstrap
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
 window.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsСontent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
-
   function hideTabContent() {
-    tabsСontent.forEach((item) => {
+    tabsСontent.forEach(item => {
       item.classList.add("hide");
       item.classList.remove("show", "fade");
     });
-
-    tabs.forEach((item) => {
+    tabs.forEach(item => {
       item.classList.remove("tabheader__item_active");
     });
   }
-
   function showTabContent(i = 0) {
     tabsСontent[i].classList.add("show", "fade");
     tabsСontent[i].classList.remove("hide");
-
     tabs[i].classList.add("tabheader__item_active");
   }
-
   hideTabContent();
   showTabContent();
-
-  tabsParent.addEventListener("click", (event) => {
+  tabsParent.addEventListener("click", event => {
     const target = event.target;
-
     if (target && target.classList.contains("tabheader__item")) {
       tabs.forEach((item, i) => {
         if (target == item) {
@@ -37,3 +34,6 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
