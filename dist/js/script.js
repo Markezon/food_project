6feb9ac89019b80fb574eb83ac526f4e060dbc55
Dmitry <neopatogen@gmail.com>
2024-11-04 04:03:30 +0100

    add ...classes

--- dist/js/script.js
+++ dist/js/script.js
@@ -130,12 +130,13 @@ window.addEventListener("DOMContentLoaded", function () {
   //class for cards
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSeltctor) {
+    constructor(src, alt, title, descr, price, parentSeltctor, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSeltctor);
       this.transfer = 27;
       this.changeToUAH();
@@ -145,19 +146,23 @@ window.addEventListener("DOMContentLoaded", function () {
     }
     render() {
       const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
       element.innerHTML = `
-          <div class="menu__item">
-            <img src=${this.src} alt=${this.alt} />
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">
-              ${this.descr}
-            </div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-              <div class="menu__item-cost">Цена:</div>
-              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
-          </div>
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">
+          ${this.descr}
+        </div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
       `;
       this.parent.append(element);
     }
--- src/js/main.js
+++ src/js/main.js
@@ -153,12 +153,13 @@ window.addEventListener("DOMContentLoaded", function () {
   //class for cards
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSeltctor) {
+    constructor(src, alt, title, descr, price, parentSeltctor, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSeltctor);
       this.transfer = 27;
       this.changeToUAH();
@@ -170,19 +171,25 @@ window.addEventListener("DOMContentLoaded", function () {
 
     render() {
       const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
       element.innerHTML = `
-          <div class="menu__item">
-            <img src=${this.src} alt=${this.alt} />
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">
-              ${this.descr}
-            </div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-              <div class="menu__item-cost">Цена:</div>
-              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
-          </div>
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">
+          ${this.descr}
+        </div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
       `;
       this.parent.append(element);
     }
