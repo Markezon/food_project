169a4d3e50d95363467c0d5eafe95bf47bbd3d31
Dmitry <neopatogen@gmail.com>
2024-11-14 19:34:21 +0100

    modified slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -146,25 +146,18 @@ window.addEventListener("DOMContentLoaded", function () {
         this.classes.forEach(className => element.classList.add(className));
       }
       element.innerHTML = `
-              <img src=${this.src} alt=${this.alt}>
-              <h3 class="menu__item-subtitle">${this.title}</h3>
-              <div class="menu__item-descr">${this.descr}</div>
-              <div class="menu__item-divider"></div>
-              <div class="menu__item-price">
-                  <div class="menu__item-cost">Цена:</div>
-                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-              </div>
-          `;
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
       this.parent.append(element);
     }
   }
-  const getResource = async url => {
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error(`Couldn't fetch ${url}, status: ${res.status}`);
-    }
-    return await res.json();
-  };
   getResource("http://localhost:3000/menu").then(data => {
     data.forEach(({
       img,
@@ -186,10 +179,10 @@ window.addEventListener("DOMContentLoaded", function () {
     failure: "Что-то пошло не так..."
   };
   forms.forEach(item => {
-    bindpostData(item);
+    bindPostData(item);
   });
   const postData = async (url, data) => {
-    const res = await fetch(url, {
+    let res = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -198,21 +191,24 @@ window.addEventListener("DOMContentLoaded", function () {
     });
     return await res.json();
   };
-  function bindpostData(form) {
+  async function getResource(url) {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+  }
+  function bindPostData(form) {
     form.addEventListener("submit", e => {
       e.preventDefault();
       let statusMessage = document.createElement("img");
       statusMessage.src = message.loading;
       statusMessage.style.cssText = `
-              display: block;
-              margin: 0 auto;
-          `;
+                display: block;
+                margin: 0 auto;
+            `;
       form.insertAdjacentElement("afterend", statusMessage);
       const formData = new FormData(form);
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
       const json = JSON.stringify(Object.fromEntries(formData.entries()));
       postData("http://localhost:3000/requests", json).then(data => {
         console.log(data);
@@ -232,11 +228,11 @@ window.addEventListener("DOMContentLoaded", function () {
     const thanksModal = document.createElement("div");
     thanksModal.classList.add("modal__dialog");
     thanksModal.innerHTML = `
-          <div class="modal__content">
-              <div class="modal__close" data-close>×</div>
-              <div class="modal__title">${message}</div>
-          </div>
-      `;
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
     document.querySelector(".modal").append(thanksModal);
     setTimeout(() => {
       thanksModal.remove();
@@ -246,47 +242,67 @@ window.addEventListener("DOMContentLoaded", function () {
     }, 4000);
   }
 
-  // fetch("http://localhost:3000/menu")
-  //   .then((data) => data.json())
-  //   .then((res) => console.log(res));
+  // Slider
 
-  //Slider
-
+  let offset = 0;
+  let slideIndex = 1;
   const slides = document.querySelectorAll(".offer__slide"),
     prev = document.querySelector(".offer__slider-prev"),
     next = document.querySelector(".offer__slider-next"),
     total = document.querySelector("#total"),
-    current = document.querySelector("#current");
-  let slideIndex = 1;
-  showSlides(slideIndex);
+    current = document.querySelector("#current"),
+    slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+    width = window.getComputedStyle(slidesWrapper).width,
+    slidesField = document.querySelector(".offer__slider-inner");
   if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
   } else {
     total.textContent = slides.length;
+    current.textContent = slideIndex;
   }
-  function showSlides(n) {
-    if (n > slides.length) {
+  slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+  slidesWrapper.style.overflow = "hidden";
+  slides.forEach(slide => {
+    slide.style.width = width;
+  });
+  next.addEventListener("click", () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+    if (slideIndex == slides.length) {
       slideIndex = 1;
+    } else {
+      slideIndex++;
     }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    slides.forEach(item => item.style.display = "none");
-    slides[slideIndex - 1].style.display = "block";
     if (slides.length < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
-  }
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  prev.addEventListener("click", () => {
-    plusSlides(-1);
   });
-  next.addEventListener("click", () => {
-    plusSlides(1);
+  prev.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
   });
 });
 /******/ })()
